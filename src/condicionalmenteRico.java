71990ddc8473bacdb3b08e46461e18c19af7fd8e
juliomorais06 <julio.fariasmorais@hotmail.com>
2023-09-16 08:55:36 -0300

    commit desafio condicionalmente rico

---
+++ src/condicionalmenteRico.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class condicionalmenteRico {
+  public static void main(String[] args) {
+
+    try (Scanner scanner = new Scanner(System.in)) {
+        int saldoTotal = scanner.nextInt();
+        int valorSaque = scanner.nextInt();
+        
+        int saldoDisponivel = saldoTotal - valorSaque;
+        
+        if(saldoTotal >= valorSaque) {
+          System.out.println("Saque realizado com sucesso. Novo saldo: "+saldoDisponivel);
+        }else {
+          System.out.println("Saldo insuficiente. Saque nao realizado!");
+        }
+    }
+
+    //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+  }
+}
